db0cc6e91c6ff38fd2c2df24cb855f1864cd8dcc
Ben Davidovich <davidovichben@gmail.com>
2024-03-29 10:30:40 +0300

    removed email required from user validation

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -11,7 +11,7 @@ class UserRequest extends FormRequest
         return [
             'first_name'    => 'required|max:30',
             'last_name'     => 'required|max:30',
-            'email'         => 'required|max:150|regex:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/',
+//            'email'         => 'required|max:150|regex:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/',
             'mobile'        => 'required|max:150|regex:/^05\\d([-]{0,1})\\d{7}$/',
             'city'          => 'required',
             'password'      => 'max:30',
