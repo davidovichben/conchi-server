949119eaad189142730c79acc167dbdd7113d925
DESKTOP-96PP9PK\DELL <rivkyrubin2@gmail.com>
2024-12-08 23:44:58 +0200

    change zip_code to maximum 9 characters

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -18,7 +18,7 @@ class UserRequest extends FormRequest
             'number'        => 'nullable|numeric|max:9999', // Numeric value for house/building number
             'apartment'     => 'nullable|numeric|max:999', // Optional numeric value for apartment number
             'floor'         => 'nullable|numeric|max:100', // Optional numeric value for floor
-            'zip_code'      => 'nullable|regex:/^[0-9]{5,7}$/', // Postal code: 5-7 numeric characters
+            'zip_code'      => 'nullable|regex:/^[0-9]{5,9}$/', // Postal code: 5-7 numeric characters
             'address_comment'  => 'nullable|max:255',
 
         ];
